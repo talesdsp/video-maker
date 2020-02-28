cc808a36eb0f315ee69e4dba8de2d31e47421d3e
Tales Pereira <talesdsp@gmail.com>
2020-02-28 19:51:55 -0300

    feat: start robot

--- index.js
+++ index.js
@@ -1,21 +1,12 @@
-const readline = require("readline-sync");
+const robots = {
+  userInput: require("./robots/user-input"),
+  text: require("./robots/text")
+};
 
-function start() {
+async function start() {
   const content = {};
-  content.searchTerm = askAndReturnSearchTerm();
-  content.prefix = askAndReturnPrefix();
-
-  function askAndReturnSearchTerm() {
-    return readline.question("Type wiki term: ");
-  }
-
-  function askAndReturnPrefix() {
-    const prefixes = ["Who is", "What is", "The history of"];
-    const selectedPrefixIndex = readline.keyInSelect(prefixes, "Choose one option");
-    const selectedPrefixText = prefixes[selectedPrefixIndex];
-
-    return selectedPrefixText;
-  }
+  robots.userInput(content);
+  await robots.text(content);
   console.log(content);
 }
 
