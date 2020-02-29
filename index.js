aa45bbaa6124bb977712db401238cdf6c8604e80
Tales Pereira <talesdsp@gmail.com>
2020-02-29 04:07:40 -0300

    feat: add google images search api

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const robots = {
   state: require("./robots/state"),
   userInput: require("./robots/user-input"),
-  text: require("./robots/text")
+  text: require("./robots/text"),
+  images: require("./robots/images")
 };
 
 async function start() {
-  robots.userInput();
-  await robots.text();
+  // robots.userInput();
+  // await robots.text();
+  await robots.images();
 
   const content = robots.state.load();
   console.dir(content, {depth: null});
